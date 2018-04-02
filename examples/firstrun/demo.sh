7310f18987cc3e6656c5a09864f82c47d15cd087
Peter <plaird@salesforce.com>
2018-04-02 13:47:46 -0600

    added missing license headers

--- examples/firstrun/demo.sh
+++ examples/firstrun/demo.sh
@@ -1,3 +1,8 @@
+# Copyright (c) 2018, salesforce.com, inc.
+# All rights reserved.
+# Licensed under the BSD 3-Clause license.
+# For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
+
 # Hello World example for the Salesforce Bazel migration tool
 
 # first do the dependency:list analysis on the sample Maven project
